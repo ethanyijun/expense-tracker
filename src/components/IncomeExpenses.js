39f952630832ffb58c7af07b02c7348febf68289
yijungai <ethan.yijun@gmail.com>
2020-05-17 12:45:34 +1000

    Showing income and expense based on the transaction amount

--- src/components/IncomeExpenses.js
+++ src/components/IncomeExpenses.js
@@ -1,15 +1,21 @@
-import React from 'react'
+import React, {useContext} from 'react'
+import { GlobalContext } from '../context/GlobalState';
 
 export const IncomeExpenses = () => {
+    const { transactions } = useContext(GlobalContext);
+    const positiveAmounts = transactions.filter(transaction => transaction.amount>0);
+    const negativeAmounts = transactions.filter(transaction => transaction.amount<0);
+    const positiveTotal = positiveAmounts.reduce((acc, item)=>(acc+item.amount),0);
+    const negativeTotal = negativeAmounts.reduce((acc, item)=>(acc+item.amount),0);
     return (
         <div className="inc-exp-container">
             <div>
                 <h4>Income</h4>
-                <p className="money plus">+$0.00</p>
+    <p className="money plus">+${positiveTotal}</p>
             </div>
             <div>
                 <h4>Expense</h4>
-                <p className="money minus">-$0.00</p>
+    <p className="money minus">-${negativeTotal}</p>
             </div>
         </div>
     )
